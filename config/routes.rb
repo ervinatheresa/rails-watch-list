4553ed519acc06f22ccdf25ec56e88b8548bd346
Ervina Theresa <ervina.theresa@gmail.com>
2022-02-24 17:05:21 +1100

    complete the validations but project is unfinished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'lists', to: 'lists#index', as: :lists
+  get 'lists/new', to: 'lists#new', as: :new_lists
+  get 'lists/:id', to: 'lists#show', as: :list
+  post 'lists', to: 'lists#create'
 end
